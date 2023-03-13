eaf408ae016a2043c4b51ef0a4ea48b2805877e1
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-13 08:45:08 +0100

    [Optional] OK But Why?

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,13 +10,17 @@ function SearchBar({ onSubmit }) {
   };
 
   const handleChange = (event) => {
-    setTerm(event.target.value);
+    setTerm(event.target.value.replace(/[a-z/, '']/, ""));
   };
 
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
+        Confirm your search: {term}
+        <br />
         <input value={term} onChange={handleChange} />
+        <br />
+        {term.length < 3 && "Term must be longer"}
       </form>
     </div>
   );
